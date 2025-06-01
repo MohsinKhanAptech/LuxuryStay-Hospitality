545050393dcb4a29cb99d81ca3409a9f15af3ebd
donatedsalt <mmohsinkhan3685@gmail.com>
2025-06-01 17:50:31 +0500

    finish useradd component layout

--- code/frontend/src/app/admin/user-add.tsx
+++ code/frontend/src/app/admin/user-add.tsx
@@ -1,7 +1,10 @@
 import { SiteHeader } from '@/components/site-header';
 import { Button } from '@/components/ui/button';
+import { Checkbox } from '@/components/ui/checkbox';
+import { Combobox } from '@/components/ui/combobox';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
+import { Link } from 'react-router';
 
 const UserAdd = () => {
   return (
@@ -24,10 +27,59 @@ const UserAdd = () => {
             <Input type='email' id='email' placeholder='Email' />
           </div>
           <div className='grid w-full items-center gap-1.5'>
-            <Label htmlFor='subject'>Subject</Label>
-            <Input type='text' id='subject' placeholder='Subject' />
+            <Label htmlFor='phone'>Phone</Label>
+            <Input type='tel' id='phone' placeholder='Phone' />
+          </div>
+          <div className='grid w-full items-center gap-1.5'>
+            <Label htmlFor='address'>Address</Label>
+            <Input type='text' id='address' placeholder='Address' />
+          </div>
+          <div className='grid w-full items-center gap-6 md:gap-4 md:grid-cols-2'>
+            <div className='grid w-full items-center gap-1.5'>
+              <Label htmlFor='userType'>User Type</Label>
+              <Combobox
+                className='w-full'
+                items={[
+                  { value: 'admin', label: 'Admin' },
+                  { value: 'manager', label: 'Manager' },
+                  { value: 'receptionist', label: 'Receptionist' },
+                  { value: 'housekeeping', label: 'Housekeeping' },
+                  { value: 'guest', label: 'Guest' },
+                ]}
+              />
+            </div>
+
+            <div className='grid w-full items-center gap-1.5'>
+              <Label htmlFor='accessLevel'>Access Level</Label>
+              <Input
+                type='number'
+                id='accessLevel'
+                placeholder='Access Level'
+              />
+            </div>
+          </div>
+          <div className='grid w-full items-center gap-1.5'>
+            <Label htmlFor='password'>Password</Label>
+            <Input type='password' id='password' placeholder='Password' />
+          </div>
+          <div className='grid w-full items-center gap-1.5'>
+            <Label htmlFor='roomType'>Preferred Room Type</Label>
+            <Input type='text' id='roomType' placeholder='Room Type' />
+          </div>
+          <div className='flex items-center gap-4 py-2'>
+            <div className='flex items-center gap-2'>
+              <Checkbox id='isActive' defaultChecked className='w-4 h-4' />
+              <Label htmlFor='isActive'>Is Active</Label>
+            </div>
+            <div className='flex items-center gap-2'>
+              <Checkbox id='smoking' className='w-4 h-4' />
+              <Label htmlFor='smoking'>Client Smokes</Label>
+            </div>
           </div>
           <Button className='w-full'>Submit</Button>
+          <Button variant={'outline'} className='w-full' asChild>
+            <Link to={'/admin/user/list'}>Back</Link>
+          </Button>
         </div>
       </section>
     </>
---
+++ code/frontend/src/components/ui/combobox.tsx
@@ -0,0 +1,77 @@
+'use client';
+
+import * as React from 'react';
+import { CheckIcon, ChevronsUpDownIcon } from 'lucide-react';
+
+import { cn } from '@/lib/utils';
+import { Button } from '@/components/ui/button';
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from '@/components/ui/command';
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover';
+
+export function Combobox({
+  items,
+  className,
+}: {
+  items: { value: string; label: string }[];
+  className?: string;
+}) {
+  const [open, setOpen] = React.useState(false);
+  const [value, setValue] = React.useState('');
+
+  return (
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger asChild>
+        <Button
+          variant='outline'
+          role='combobox'
+          aria-expanded={open}
+          className={cn('w-[200px] justify-between', className)}
+        >
+          {items && value
+            ? items.find((item) => item.value === value)?.label
+            : 'Select item...'}
+          <ChevronsUpDownIcon className='ml-2 h-4 w-4 shrink-0 opacity-50' />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className='w-[200px] p-0'>
+        <Command>
+          <CommandInput placeholder='Search item...' />
+          <CommandList>
+            <CommandEmpty>No item found.</CommandEmpty>
+            <CommandGroup>
+              {items.map((item) => (
+                <CommandItem
+                  key={item.value}
+                  value={item.value}
+                  onSelect={(currentValue) => {
+                    setValue(currentValue === value ? '' : currentValue);
+                    setOpen(false);
+                  }}
+                >
+                  <CheckIcon
+                    className={cn(
+                      'mr-2 h-4 w-4',
+                      value === item.value ? 'opacity-100' : 'opacity-0'
+                    )}
+                  />
+                  {item.label}
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          </CommandList>
+        </Command>
+      </PopoverContent>
+    </Popover>
+  );
+}
