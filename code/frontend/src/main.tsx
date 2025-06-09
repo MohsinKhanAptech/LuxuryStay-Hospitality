dd459adb6616c723d503aaff4ee0245f74423945
donatedsalt <mmohsinkhan3685@gmail.com>
2025-06-09 18:15:26 +0500

    add userdetail path to app

--- code/frontend/src/main.tsx
+++ code/frontend/src/main.tsx
@@ -1,39 +1,41 @@
-import { createRoot } from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router';
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router";
 
-import './index.css';
-import App from './App.tsx';
-import AdminLayout from './app/admin/admin-layout.tsx';
-import Dashboard from './app/admin/dashboard.tsx';
-import UserList from './app/admin/user-list.tsx';
-import UserAdd from './app/admin/user-add.tsx';
-import UserEdit from './app/admin/user-edit.tsx';
+import "./index.css";
+import App from "./App.tsx";
+import AdminLayout from "./app/admin/admin-layout.tsx";
+import Dashboard from "./app/admin/dashboard.tsx";
+import UserList from "./app/admin/user-list.tsx";
+import UserDetail from "./app/admin/user-detail.tsx";
+import UserAdd from "./app/admin/user-add.tsx";
+import UserEdit from "./app/admin/user-edit.tsx";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     children: [{ index: true, Component: App }],
   },
   {
-    path: '/admin',
+    path: "/admin",
     Component: AdminLayout,
     children: [
       { index: true, Component: Dashboard },
-      { path: 'dashboard', Component: Dashboard },
+      { path: "dashboard", Component: Dashboard },
       {
-        path: 'user',
+        path: "user",
         children: [
           { index: true, Component: UserList },
-          { path: 'list', Component: UserList },
-          { path: 'add', Component: UserAdd },
-          { path: 'edit', Component: UserEdit },
+          { path: "list", Component: UserList },
+          { path: "detail", Component: UserDetail },
+          { path: "add", Component: UserAdd },
+          { path: "edit", Component: UserEdit },
         ],
       },
-      { path: 'users', Component: UserList },
+      { path: "users", Component: UserList },
     ],
   },
 ]);
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <RouterProvider router={router} />
 );
