eac4da366770d98507bd70e9bd1c04ade00081a3
donatedsalt <mmohsinkhan3685@gmail.com>
2025-06-11 12:19:13 +0500

    remove table from dashboard

--- code/frontend/src/app/admin/dashboard.tsx
+++ code/frontend/src/app/admin/dashboard.tsx
@@ -1,22 +1,22 @@
-import { ChartAreaInteractive } from '@/components/chart-area-interactive';
-import { SectionCards } from '@/components/section-cards';
-import { SiteHeader } from '@/components/site-header';
-import { DataTable } from '@/components/data-table';
+import { ChartAreaInteractive } from "@/components/chart-area-interactive";
+import { SectionCards } from "@/components/section-cards";
+import { SiteHeader } from "@/components/site-header";
+// import { DataTable } from "@/components/data-table";
 
-import data from './data.json';
+// import data from "./data.json";
 
 function Dashboard() {
   return (
     <>
-      <SiteHeader title={'Dashboard'} />
-      <div className='flex flex-1 flex-col'>
-        <div className='@container/main flex flex-1 flex-col gap-2'>
-          <div className='flex flex-col gap-4 py-4 md:gap-6 md:py-6'>
+      <SiteHeader title={"Dashboard"} />
+      <div className="flex flex-col flex-1">
+        <div className="@container/main flex flex-1 flex-col gap-2">
+          <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
             <SectionCards />
-            <div className='px-4 lg:px-6'>
+            <div className="px-4 lg:px-6">
               <ChartAreaInteractive />
             </div>
-            <DataTable data={data} />
+            {/* <DataTable data={data} /> */}
           </div>
         </div>
       </div>
