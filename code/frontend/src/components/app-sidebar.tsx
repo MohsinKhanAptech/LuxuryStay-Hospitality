68b862d487051e57447dfacee185032558ff67e0
donatedsalt <mmohsinkhan3685@gmail.com>
2025-06-11 12:24:03 +0500

    fixed up sidebar

--- code/frontend/src/components/app-sidebar.tsx
+++ code/frontend/src/components/app-sidebar.tsx
@@ -1,17 +1,17 @@
-import * as React from 'react';
+import * as React from "react";
 import {
   IconDashboard,
   IconDoor,
-  IconHelp,
-  IconSearch,
-  IconSettings,
+  // IconHelp,
+  // IconSearch,
+  // IconSettings,
   IconTools,
   IconUsers,
-} from '@tabler/icons-react';
+} from "@tabler/icons-react";
 
-import { NavMain } from '@/components/nav-main';
-import { NavSecondary } from '@/components/nav-secondary';
-import { NavUser } from '@/components/nav-user';
+import { NavMain } from "@/components/nav-main";
+// import { NavSecondary } from "@/components/nav-secondary";
+import { NavUser } from "@/components/nav-user";
 import {
   Sidebar,
   SidebarContent,
@@ -20,70 +20,70 @@ import {
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
-} from '@/components/ui/sidebar';
-import {  StarsIcon } from 'lucide-react';
-import { Link } from 'react-router';
+} from "@/components/ui/sidebar";
+import { StarsIcon } from "lucide-react";
+import { Link } from "react-router";
 
 const data = {
   user: {
-    name: 'shadcn',
-    email: 'm@example.com',
-    avatar: '/avatars/shadcn.jpg',
+    name: "Admin",
+    email: "admin@example.com",
+    avatar: "/avatars/user.jpg",
   },
   navMain: [
     {
-      title: 'Dashboard',
-      url: '/admin',
+      title: "Dashboard",
+      url: "/admin",
       icon: IconDashboard,
     },
     {
-      title: 'Users',
-      url: '/admin/user/list',
+      title: "Users",
+      url: "/admin/user/list",
       icon: IconUsers,
     },
     {
-      title: 'Rooms',
-      url: '/admin/room/list',
+      title: "Rooms",
+      url: "/admin/room/list",
       icon: IconDoor,
     },
     {
-      title: 'Housekeepings',
-      url: '/admin/housekeeping/list',
+      title: "Housekeepings",
+      url: "/admin/housekeeping/list",
       icon: IconTools,
     },
   ],
-  navSecondary: [
-    {
-      title: 'Settings',
-      url: '#',
-      icon: IconSettings,
-    },
-    {
-      title: 'Get Help',
-      url: '#',
-      icon: IconHelp,
-    },
-    {
-      title: 'Search',
-      url: '#',
-      icon: IconSearch,
-    },
-  ],
+  // navSecondary: [
+  //   {
+  //     title: "Settings",
+  //     url: "#",
+  //     icon: IconSettings,
+  //   },
+  //   {
+  //     title: "Get Help",
+  //     url: "#",
+  //     icon: IconHelp,
+  //   },
+  //   {
+  //     title: "Search",
+  //     url: "#",
+  //     icon: IconSearch,
+  //   },
+  // ],
 };
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   return (
-    <Sidebar collapsible='offcanvas' {...props}>
+    <Sidebar collapsible="offcanvas" {...props}>
       <SidebarHeader>
         <SidebarMenu>
           <SidebarMenuItem>
             <SidebarMenuButton
               asChild
-              className='data-[slot=sidebar-menu-button]:!p-1.5'
+              className="data-[slot=sidebar-menu-button]:!p-1.5"
             >
-              <Link to={'/admin'}>
-                <StarsIcon className='!size-5' />
-                <span className='text-base font-semibold'>LuxuryStar</span>
+              <Link to={"/admin"}>
+                <StarsIcon className="!size-5" />
+                <span className="text-base font-semibold">LuxuryStar</span>
               </Link>
             </SidebarMenuButton>
           </SidebarMenuItem>
@@ -91,7 +91,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
       </SidebarHeader>
       <SidebarContent>
         <NavMain items={data.navMain} />
-        <NavSecondary items={data.navSecondary} className='mt-auto' />
+        {/* <NavSecondary items={data.navSecondary} className="mt-auto" /> */}
       </SidebarContent>
       <SidebarFooter>
         <NavUser user={data.user} />
