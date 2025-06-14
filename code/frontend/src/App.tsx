d6c7cbaf80d9b751c27d5cfebaf91fd2b59babbe
donatedsalt <mmohsinkhan3685@gmail.com>
2025-06-14 17:33:59 +0500

    fix layout on home page

--- code/frontend/src/App.tsx
+++ code/frontend/src/App.tsx
@@ -4,9 +4,8 @@ import { Button } from "@/components/ui/button";
 
 function App() {
   return (
-    <div className="h-screen w-screen flex flex-col justify-center items-center">
+    <div className="flex flex-col items-center justify-center w-screen h-screen gap-4">
       <h1 className="text-5xl font-bold">LuxuryStar Hospitality</h1>
-      <br />
 
       <Button asChild>
         <Link to={"/admin/login"}>Login to Dashboard</Link>
